cff91d4d0dcaf29bd0b3c00b6693e0c7c5c91bf0
rogue <retrokarr@gmail.com>
2018-02-23 23:04:33 +0900

    180223

---
+++ src/com/retrokarr/Q11066/Main.java
@@ -0,0 +1,43 @@
+package com.retrokarr.Q11066;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FS sc = new FS();
+
+        int testCase = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        while(testCase-- > 0) {
+            int num = sc.nextInt();
+            int[] nums = new int[num];
+
+            for(int i = 0 ; i < num ; ++i)
+                nums[i] = sc.nextInt();
+
+            sb.append(solve(nums)).append('\n');
+        }
+
+    }
+
+    static int solve(int[] nums) {
+
+        return 1;
+    }
+
+    static class FS {
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st = null;
+
+        int nextInt() throws IOException {
+            if(st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+
+            return Integer.parseInt(st.nextToken());
+        }
+    }
+}
