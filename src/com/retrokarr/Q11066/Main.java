f55653aebc0f8708d4233a95642a523e64cb1149
retrokarr <retrokarr@gmail.com>
2018-02-28 23:27:56 +0900

    180228

--- src/com/retrokarr/Q11066/Main.java
+++ src/com/retrokarr/Q11066/Main.java
@@ -3,9 +3,12 @@ package com.retrokarr.Q11066;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] cache;
+    static int[] nums;
     public static void main(String[] args) throws IOException {
         FS sc = new FS();
 
@@ -14,22 +17,38 @@ public class Main {
 
         while(testCase-- > 0) {
             int num = sc.nextInt();
-            int[] nums = new int[num];
+            nums = new int[num];
+            cache = new int[num][num];
 
-            for(int i = 0 ; i < num ; ++i)
+            for(int i = 0 ; i < num ; ++i) {
                 nums[i] = sc.nextInt();
+                Arrays.fill(cache[i], -1);
+                cache[i][i] = nums[i];
+            }
 
-            sb.append(solve(nums)).append('\n');
+            sb.append(DFS(0, num - 1)).append('\n');
+
+            for(int i = 0 ; i < num ; ++i) {
+                for (int j = 0; j < num; ++j)
+                    System.out.printf("%4d", cache[i][j]);
+                System.out.println();
+            }
         }
-
+        System.out.println(sb);
     }
 
     /*
     연쇄 행렬 곱셈 알고리즘
     matrix chain multiplication
      */
-    static int solve(int[] nums) {
-        return 1;
+    static int DFS(int y, int x) {
+        if(cache[y][x] > -1)
+            return cache[y][x];
+
+        if(y == x)
+            return cache[y][x] = nums[y];
+
+        return cache[y][x] = Math.min(2 * DFS(y, x - 1) + nums[x], 2 * DFS(y + 1, x) + nums[nums.length - 1 - y]);
     }
 
     static class FS {
