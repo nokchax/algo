60ee814920b1a269448c2195253d2482747a431a
rogue <retrokarr@gmail.com>
2018-05-10 21:59:31 +0900

    queue

---
+++ src/com/retrokarr/Q11866/Main.java
@@ -0,0 +1,39 @@
+package com.retrokarr.Q11866;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        Queue<Integer> q = new LinkedList<>();
+        List<String> answer = new ArrayList<>();
+
+        int nums = Integer.parseInt(inputs[0]);
+        int skip = Integer.parseInt(inputs[1]);
+
+        for(int i = 1 ; i <= nums ; ++i)
+            q.add(i);
+
+        while(!q.isEmpty()) {
+            for(int i = 1 ; i < skip ; ++i)
+                q.add(q.poll());
+
+            answer.add(String.valueOf(q.poll()));
+        }
+
+        System.out.println("<" + String.join(", ", answer) + ">");
+    }
+}
+/**
+     front += skip;
+
+     if(front + skip > arr.size()){
+         front %= arr.size();
+     }
+
+     sb.append(arr.remove(front) + ", ");
+ */
