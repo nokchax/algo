ebe7fdf69f15c92cf54c526698d4b5f5b4111c40
rogue <retrokarr@gmail.com>
2018-05-12 03:39:06 +0000

    test

--- src/com/retrokarr/Q00000/Main.java
+++ src/com/retrokarr/Q00000/Main.java
@@ -3,6 +3,5 @@ package com.retrokarr.Q00000;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("TEST!");
     }
 }
