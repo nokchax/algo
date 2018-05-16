aba328d3a2fa370795840f9e3a75394cf870a8fa
rogue <retrokarr@gmail.com>
2018-05-16 16:35:15 +0900

    dp

--- src/com/retrokarr/Q01932/Main.java
+++ src/com/retrokarr/Q01932/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
@@ -15,9 +15,19 @@ public class main {
             String[] inputs = br.readLine().split(" ");
 
             for(int j = 0 ; j < inputs.length ; ++j) {
+                int value = Integer.parseInt(inputs[j]);
+                int leftTop = j == 0 ? j : j - 1;
+                int rightTop = j < inputs.length - 1 ? j : j - 1;
 
-
+                cache[i][j] = value + Math.max(cache[i - 1][leftTop], cache[i - 1][rightTop]);
             }
         }
+
+        int ret = 0;
+        size--;
+        for(int i = 0 ; i <= size ; ++i)
+            ret = Math.max(ret, cache[size][i]);
+
+        System.out.println(ret);
     }
 }
