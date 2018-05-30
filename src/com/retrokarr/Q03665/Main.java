37bad16f13557a72aa7ef9b5cfe828bfbc5fad69
rogue <retrokarr@gmail.com>
2018-05-31 07:10:06 +0900

    topological sort

--- src/com/retrokarr/Q03665/Main.java
+++ src/com/retrokarr/Q03665/Main.java
@@ -19,70 +19,76 @@ public class Main {
         int numOfTeam = Integer.parseInt(br.readLine());
         int[] team = new int[numOfTeam + 1];
         int[] in = new int[numOfTeam + 1];
-        ArrayList<Integer>[] nextTeam = new ArrayList[numOfTeam + 1];
+        boolean[][] relation = new boolean[numOfTeam + 1][numOfTeam + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 1; i <= numOfTeam ; ++i) {
-            nextTeam[i] = new ArrayList<>();
             team[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 1 ; i <= numOfTeam ; ++i)
             for(int j = i + 1 ; j <= numOfTeam ; ++j) {
-                nextTeam[team[i]].add(team[j]);
+                relation[team[i]][team[j]] = true;
                 in[team[j]]++;
             }
         //end of init
 
-        boolean impossible = false;
         int data = Integer.parseInt(br.readLine());
         while(data-- > 0) {
             st = new StringTokenizer(br.readLine());
             int back = Integer.parseInt(st.nextToken());
             int fore = Integer.parseInt(st.nextToken());
 
-            if(!nextTeam[fore].contains(back))
-                impossible = true;
-            else
-                nextTeam[fore].remove(nextTeam[fore].indexOf(back));
-
-            if(--in[back] < 0)
-                impossible = true;
-
-            nextTeam[back].add(fore);
-            in[fore]++;
+            if(relation[back][fore]) {
+                in[fore]--;
+                in[back]++;
+                relation[back][fore] = false;
+                relation[fore][back] = true;
+            } else {
+                in[fore]++;
+                in[back]--;
+                relation[back][fore] = true;
+                relation[fore][back] = false;
+            }
         }
 
-        if(impossible)
-            return "IMPOSSIBLE";
-
         Queue<Integer> q = new LinkedList<>();
+
         for(int i = 1 ; i <= numOfTeam ; ++i)
-            if(in[i] == 0) {
-                if(q.isEmpty())
-                    q.add(i);
-                else
-                    return "?";
-            }
+            if(in[i] == 0)
+                q.add(i);
 
         StringBuilder sb = new StringBuilder();
-        while(!q.isEmpty()) {
+        int answer = 1;
+        for(int i = 1 ; i <= numOfTeam ; ++i) {
+            if(q.isEmpty()) {
+                answer = 0;
+                break;
+            }
+
+            if(q.size() > 1) {
+                answer = 2;
+                break;
+            }
+
             int curTeam = q.poll();
             sb.append(curTeam + " ");
 
-            for(int next : nextTeam[curTeam]) {
-                if(--in[next] == 0)
-                    q.add(next);
-
-                if(in[next] < 0)
-                    return "IMPOSSIBLE";
-            }
+            for(int j = 1 ; j <= numOfTeam ; ++j)
+                if(relation[curTeam][j])
+                    if(--in[j] == 0)
+                        q.add(j);
         }
 
-        for(int i = 1 ; i <= numOfTeam ; ++i)
-            if(in[i] != 0)
+        switch (answer) {
+            case 1:
+                return sb.toString();
+            case 2:
+                return "?";
+            case 0:
                 return "IMPOSSIBLE";
+        }
 
-        return sb.toString();
+        return "";
     }
 }
