8da6be8629386ac883638af7598ea4dc192dbdb5
rogue <retrokarr@gmail.com>
2018-06-06 21:57:54 +0900

    palindrome

---
+++ src/com/retrokarr/Q01254/Main.java
@@ -0,0 +1,12 @@
+package com.retrokarr.Q01254;
+
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
