a9778f5ac1d35045e0049477ddeb15b8c140b318
rogue <retrokarr@gmail.com>
2018-06-07 15:13:07 +0900

    palindrome

--- src/com/retrokarr/Q01254/Main.java
+++ src/com/retrokarr/Q01254/Main.java
@@ -7,6 +7,25 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
+        String str = br.readLine();
 
+        for(int i = str.length() ; ; ++i) {
+            boolean isPelindrome = true;
+
+            for(int j = 0 ; j < str.length() ; ++j) {
+                if(i - j - 1 < str.length() && str.charAt(j) != str.charAt(i - j - 1)) {
+                    isPelindrome = false;
+                    break;
+                }
+            }
+
+            if(isPelindrome) {
+                bw.write(String.valueOf(i));
+                break;
+            }
+        }
+
+        bw.close();
+        br.close();
     }
 }
