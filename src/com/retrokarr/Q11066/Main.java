56742e5323a5fff3564cdbe62799b38b97f6b6df
rogue <retrokarr@gmail.com>
2018-06-08 23:15:47 +0900

    dp

--- src/com/retrokarr/Q11066/Main.java
+++ src/com/retrokarr/Q11066/Main.java
@@ -1,77 +1,41 @@
 package com.retrokarr.Q11066;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] cache;
-    static int[] sum;
-    static int[] nums;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        FS sc = new FS();
+        int testCase = Integer.parseInt(br.readLine());
 
-        int testCase = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
+        while(testCase-- > 0)
+            bw.write(solve() + "\n");
 
-        while(testCase-- > 0) {
-            int num = sc.nextInt();
-            nums = new int[num];
-            cache = new int[num][num];
-            sum = new int[num];
-            int tempSum = 0;
-
-            for(int i = 0 ; i < num ; ++i) {
-                Arrays.fill(cache[i], -1);
-                cache[i][i] = 0;
-                nums[i] = sc.nextInt();
-                tempSum += nums[i];
-                sum[i] = tempSum;
-            }
-
-            sb.append(DFS(0, num - 1)).append('\n');
-
-            for(int i = 0 ; i < num ; ++i) {
-                for (int j = 0; j < num; ++j)
-                    System.out.printf("%4d", cache[i][j]);
-                System.out.println();
-            }
-        }
-        System.out.println(sb);
+        bw.close();
+        br.close();
     }
 
-    /*
-    연쇄 행렬 곱셈 알고리즘
-    matrix chain multiplication
-     */
-    static int DFS(int y, int x) {
-        if(cache[y][x] > -1)
-            return cache[y][x];
+    static String solve() throws IOException {
+        int nChapters = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] sum = new int[nChapters + 1];
+        int[][] cache = new int[nChapters + 1][nChapters + 1];
 
-        int min = Math.min(y, x);
-        int max = Math.max(y, x);
-        int temp = Integer.MAX_VALUE;
-        for(int k = min ; k < max ; ++k) {
-            int start = min > 0 ? sum[min] : 0;
-            int target = DFS(y, k) + DFS(k + 1, x) + sum[max] - start;
-            temp = Math.min(temp, target);
+        for(int i = 1 ; i <= nChapters ; ++i)
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+
+        for(int i = 2 ; i <= nChapters ; ++i) {
+            for(int j = i - 1 ; j > 0 ; --j) {
+                cache[j][i] = 987654321;
+                for(int k = j ; k < i ; ++k)
+                    cache[j][i] = Math.min(cache[j][i], cache[j][k] + cache[k + 1][i]);
+
+                cache[j][i] += sum[i] - sum[j - 1];
+            }
         }
 
-        //sum에 대한 추가적인 dp..
-        return cache[y][x] = temp;
-    }
-
-    static class FS {
-        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        private StringTokenizer st = null;
-
-        int nextInt() throws IOException {
-            if(st == null || !st.hasMoreTokens())
-                st = new StringTokenizer(br.readLine());
-
-            return Integer.parseInt(st.nextToken());
-        }
+        return String.valueOf(cache[1][nChapters]);
     }
 }
