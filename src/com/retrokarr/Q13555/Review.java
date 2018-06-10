15c9f96dc147695131199f6e7d9eeb0634cc2059
rogue <retrokarr@gmail.com>
2018-06-10 21:23:21 +0900

    lis fenwick tree

--- src/com/retrokarr/Q13555/Review.java
+++ src/com/retrokarr/Q13555/Review.java
@@ -1,10 +1,10 @@
-package com.retrokarr.Q13556;
+package com.retrokarr.Q13555;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Review {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -15,28 +15,21 @@ public class Main {
         int[] cache = new int[N + 1];
         int[] num = new int[N + 1];
         int[] bit = new int[100001];
-        int[] visit = new int[100001];
         int MOD = 5000000;
 
         Arrays.fill(cache, 1);
         st = new StringTokenizer(br.readLine());
-        for(int i = 1 ; i <= N ; ++i) {
+        for(int i = 1 ; i <= N ; ++i)
             num[i] = Integer.parseInt(st.nextToken());
-            visit[num[i]]++;
-            if(visit[num[i]] > 1) cache[i] = 0;
-        }
 
         for(int i = 2 ; i <= K ; ++i) {
             Arrays.fill(bit, 0);
-            Arrays.fill(visit, 0);
-
             for(int j = 1 ; j <= N ; ++j) {
                 for(int x = num[j] ; x <= 100000 ; x += (x & -x))
                     bit[x] = (bit[x] + cache[j]) % MOD;
-                cache[j] = (MOD - visit[num[j]]) % MOD;
+                cache[j] = 0;
                 for(int x = num[j] - 1 ; x > 0 ; x -= (x & -x))
                     cache[j] = (bit[x] + cache[j]) % MOD;
-                visit[num[j]] = (visit[num[j]] + cache[j]) % MOD;
             }
         }
 
